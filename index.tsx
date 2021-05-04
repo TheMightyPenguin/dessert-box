dd45f706c86bf09e7cf1e4d2116c5e92e7464087
Victor Tortolero <victor.tortolero@medwing.com>
2021-05-03 21:20:31 -0500

    Update API to use atoms.properties

--- index.tsx
+++ index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import type * as Polymorphic from '@radix-ui/react-polymorphic';
 
-export function createBox<AtomsFn extends (...args: any[]) => string>(atomsFn: AtomsFn, usedProperties: string[]) {
+interface AtomsFnBase {
+  (...args: any): string;
+  properties: Set<string>;
+}
+
+export function createBox<AtomsFn extends AtomsFnBase>(atomsFn: AtomsFn) {
   type BoxProps = {
     as?: keyof JSX.IntrinsicElements;
     children: any;
+    className?: string;
   } & Parameters<AtomsFn>[0];
-
-  const usedPropertiesSet = new Set(usedProperties);
 
   function Box({
     as: Element = 'div',
@@ -20,7 +24,7 @@ export function createBox<AtomsFn extends (...args: any[]) => string>(atomsFn: A
     let otherProps: Record<string, unknown> = {};
 
     Object.entries(props).map(([name, value]) => {
-      if (usedPropertiesSet.has(name)) {
+      if (atomsFn.properties.has(name)) {
         hasAtomProps = true;
         atomProps[name] = value;
       } else {
@@ -35,6 +39,42 @@ export function createBox<AtomsFn extends (...args: any[]) => string>(atomsFn: A
           hasAtomProps || className
             ? `${className ?? ''}${hasAtomProps && className ? ' ' : ''}${
                 hasAtomProps ? atomsFn(atomProps) : ''
+              }`
+            : undefined
+        }
+      >
+        {children}
+      </Element>
+    );
+  }
+
+  return Box as Polymorphic.ForwardRefComponent<'div', Omit<BoxProps, 'as'>>;
+}
+
+export function createBoxWithAtomsProp<AtomsFn extends AtomsFnBase>(atomsFn: AtomsFn) {
+  type BoxProps = {
+    as?: keyof JSX.IntrinsicElements;
+    children: any;
+    className?: string;
+    atoms?: Parameters<AtomsFn>[0]
+  };
+
+  function Box({
+    as: Element = 'div',
+    className,
+    children,
+    atoms,
+    ...props
+  }: BoxProps) {
+    const hasAtomProps = typeof atoms !== 'undefined';
+
+    return (
+      <Element
+        {...props}
+        className={
+          hasAtomProps || className
+            ? `${className ?? ''}${hasAtomProps && className ? ' ' : ''}${
+                hasAtomProps ? atomsFn(atoms) : ''
               }`
             : undefined
         }
