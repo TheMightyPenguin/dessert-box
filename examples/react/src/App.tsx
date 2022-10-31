f308fc95324226d3f3876c250bdf5f7cc688f8e5
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-11-01 00:49:15 +0100

    refactor: use vite for react example with local alias for better DX / easier testing

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createBox, createBoxWithAtomsProp } from '@dessert-box/react';
+import React, { useRef } from 'react';
+import { createBoxWithAtomsProp } from '@dessert-box/react';
 import { themeClass, atoms } from './atoms.css';
 import Box from './Box';
 import Text from './Text/Text';
@@ -12,8 +11,9 @@ const AsBoxButton = (
   props: React.ComponentProps<typeof Box> & React.ComponentProps<typeof Button>,
 ) => <Box as={Button} {...props} />;
 
-const App = () => {
-  const [el, setEl] = React.useState<HTMLElement>();
+export const App = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <Box className={themeClass}>
       <AsBoxButton background="yellow" padding="extraLarge">
@@ -21,7 +21,7 @@ const App = () => {
       </AsBoxButton>
       <Box
         as="a"
-        ref={setEl}
+        ref={ref}
         href="https://google.com"
         padding={{ desktop: 'extraLarge', mobile: 'small' }}
       >
@@ -64,8 +64,3 @@ const App = () => {
     </Box>
   );
 };
-
-const container = document.createElement('div');
-document.body.appendChild(container);
-
-ReactDOM.render(<App />, container);
